e22c155acdb963e4379fd603047aed76fb4601ce
Jianyong Chen <baluschch@gmail.com>
2022-08-24 23:58:32 +0800

    Replace boost::string_view with std::string_view.
    Note that the constructor of boost::string_view may diff
    from that of std::string_view.
    
    Signed-off-by: Jianyong Chen <baluschch@gmail.com>

--- include/boost/url/string_view.hpp
+++ include/boost/url/string_view.hpp
@@ -11,22 +11,73 @@
 #define BOOST_URL_STRING_VIEW_HPP
 
 #include <boost/url/detail/config.hpp>
-#include <boost/core/detail/string_view.hpp>
+#ifndef BOOST_URL_STANDALONE
+# include <boost/utility/string_view.hpp>
+# ifndef BOOST_NO_CXX17_HDR_STRING_VIEW
+#  include <string_view>
+# endif
+#else
+# if __has_include(<string_view>)
+#  include <string_view>
+#  if __cpp_lib_string_view < 201603L
+#   error Support for std::string_view is required to use Boost.URL standalone
+#  endif
+# else
+#  error Header <string_view> is required to use Boost.URL standalone
+# endif
+#endif
+#include <type_traits>
 
 namespace boost {
 namespace urls {
 
-/** The type of string_view used by the library
+#ifdef BOOST_URL_DOCS
 
-    String views are used to pass character
-    buffers into or out of functions. Ownership
-    of the underlying character buffer is not
-    transferred; the caller is responsible for
-    ensuring that the lifetime of the object
-    owning the character buffer extends until
-    the string view is no longer referenced.
+/** The type of string view used by the library.
+
+    This type alias is set depending
+    on how the library is configured:
+
+    @par Use with Boost
+
+    If the macro `BOOST_URL_STANDALONE` is
+    not defined, this type will be an alias
+    for `boost::string_view`.
+    Compiling a program using the library will
+    require Boost, and a compiler conforming
+    to C++11 or later.
+
+    @par Use without Boost
+
+    If the macro `BOOST_URL_STANDALONE` is
+    defined, this type will be an alias
+    for `std::string_view`.
+    Compiling a program using the library will
+    require only a compiler conforming to C++17
+    or later.
+
+    @see https://en.cppreference.com/w/cpp/string/basic_string_view
 */
-typedef boost::core::string_view string_view;
+using string_view = __see_below__;
+
+#elif !defined(BOOST_URL_STANDALONE)
+
+using string_view = boost::string_view;
+
+#else
+
+using string_view = std::string_view;
+
+#endif
+
+namespace detail {
+
+template <class T>
+using is_string_viewish = typename std::enable_if<
+    std::is_convertible<T const &, string_view>::value &&
+    !std::is_convertible<T const &, char const *>::value>::type;
+
+} // detail
 
 } // urls
 } // boost
