89436ba8bf23c15e651833ca35772152cdfa506d
Jianyong Chen <baluschch@gmail.com>
2022-08-25 00:22:03 +0800

    Replace boost::system::error_XXX with std::error_XXX.
    Signed-off-by: Jianyong Chen <baluschch@gmail.com>

--- include/boost/url/error_code.hpp
+++ include/boost/url/error_code.hpp
@@ -11,8 +11,12 @@
 #define BOOST_URL_ERROR_CODE_HPP
 
 #include <boost/url/detail/config.hpp>
-#include <boost/system/error_code.hpp>
-#include <boost/system/system_error.hpp>
+#ifndef BOOST_URL_STANDALONE
+# include <boost/system/error_code.hpp>
+# include <boost/system/system_error.hpp>
+#else
+# include <system_error>
+#endif
 
 namespace boost {
 namespace urls {
@@ -20,6 +24,8 @@ namespace urls {
 #ifndef BOOST_URL_DOCS
 namespace error_types {
 #endif
+
+#ifndef BOOST_URL_STANDALONE
 
 /// The type of error category used by the library
 using error_category = boost::system::error_category;
@@ -47,14 +53,39 @@ error_category const& system_category();
 using boost::system::system_category;
 #endif
 
+#else
+
+using error_code = std::error_code;
+using error_category = std::error_category;
+using error_condition = std::error_condition;
+using system_error = std::system_error;
+
+/// A function to return the generic error category used by the library
+#ifdef BOOST_URL_DOCS
+error_category const& generic_category();
+#else
+using std::generic_category;
+#endif
+
+/// A function to return the system error category used by the library
+#if BOOST_URL_DOCS
+error_category const& system_category();
+#else
+using std::system_category;
+#endif
+
+#endif
+
 /// The set of constants used for cross-platform error codes
 #if BOOST_URL_DOCS
 enum errc
 {
     __see_below__
 };
-#else
+#elif !defined(BOOST_URL_STANDALONE)
 namespace errc = boost::system::errc;
+#else
+using errc = std::errc;
 #endif
 
 #ifndef BOOST_URL_DOCS
