f4b8b193c62dbd821d122a7a2aa507db04ddf86a
Jianyong Chen <baluschch@gmail.com>
2022-08-25 11:13:04 +0800

    Replace boost::variant2 with std::variant.
    Signed-off-by: Jianyong Chen <baluschch@gmail.com>

--- include/boost/url/variant.hpp
+++ include/boost/url/variant.hpp
@@ -11,16 +11,26 @@
 #define BOOST_URL_VARIANT_HPP
 
 #include <boost/url/detail/config.hpp>
-#include <boost/variant2/variant.hpp>
+#ifndef BOOST_URL_STANDALONE
+# include <boost/variant2/variant.hpp>
+#else
+# include<variant>
+#endif
 
 namespace boost {
 namespace urls {
 
 /** The type of variant used by the library
 */
+
+#ifndef BOOST_URL_STANDALONE
 template<class... Ts>
 using variant =
     boost::variant2::variant<Ts...>;
+#else
+template<class... Ts>
+using variant = std::variant<Ts...>;
+#endif
 
 } // urls
 } // boost
